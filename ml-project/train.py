56c5d6dd345b90e0610f1cd8b40f7d247ec82097
Paul Schlueter <paul@paulschlueter.com>
2019-08-19 14:09:43 -0500

    Built MLProject Docker Environment

---
+++ ml-project/train.py
@@ -0,0 +1,33 @@
+import numpy as np
+import os
+from sklearn import datasets
+from sklearn.externals import joblib
+from sklearn.ensemble import IsolationForest
+
+def generate_data():
+    outliers_fraction = 0.15
+    n_samples = 1000
+
+    n_outliers = int(outliers_fraction * n_samples)
+    n_inliers = n_samples - n_outliers
+
+    # Generate inliers
+    inliers = datasets.make_blobs(
+        n_samples=n_inliers,
+        n_features=2,
+        centers=[[2, 2], [-2, -2]],
+        cluster_std=[0.5, 0.5],
+        random_state=0
+    )[0]
+
+    # Generate outliers
+    rng = np.random.RandomState(13)
+    outliers = rng.uniform(low=-6, high=6, size=(n_outliers, 2))
+
+    return np.concatenate([inliers, outliers], axis=0)
+
+if __name__ == '__main__':
+    model = IsolationForest(n_jobs=-1, contamination=0.15)
+    model.fit(generate_data())
+    joblib.dump(model, os.path.join('/mlflow', 'model.joblib'))
+    print('DONE!')
